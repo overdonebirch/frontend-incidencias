ba7a93d42518ac8daf8d57e1e724fa5b44506a92
overdonebirch <alendez19@gmai.com>
2025-04-01 21:18:50 +0200

    primer commit, añado vuetify

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App);
+
+const vuetify = createVuetify({
+    components,
+    directives
+})
+
+app.use(vuetify)
+
+app.mount('#app');
