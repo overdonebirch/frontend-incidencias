954695cd52bf16aa0910631f892a8b074e23c8d7
overdonebirch <alendez19@gmai.com>
2025-04-10 13:13:38 +0200

    le quito el v-list

--- src/components/LeftSideBar.vue
+++ src/components/LeftSideBar.vue
@@ -5,9 +5,9 @@
 
 <template>
     <v-navigation-drawer color="cyan-lighten-2" height="100vh" permanent class="py-10">
-        <v-list nav class="d-flex flex-column align-center gap-5">
+
             <slot></slot>
-        </v-list>
+
     </v-navigation-drawer>
 </template>
 
