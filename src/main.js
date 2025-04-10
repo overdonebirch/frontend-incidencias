a30532173599ba44a6a7889a6964cf91f15355ed
overdonebirch <alendez19@gmai.com>
2025-04-10 14:06:47 +0200

    añado el codigo para ver los iconos de vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createVuetify} from 'vuetify'
 import {createPinia} from 'pinia'
 import './style.css'
 import App from './App.vue'
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import router from './router'
 
@@ -10,10 +11,16 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 
+
+
+
 const vuetify = createVuetify({
     components,
-    directives
-})
+    directives,
+    theme: {
+        defaultTheme: 'light'
+      }
+  })
 const pinia = createPinia();
 
 
