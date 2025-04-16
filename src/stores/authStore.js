3af8fb20b06542bca2470f4d61e9fe2cab64712b
overdonebirch <alendez19@gmai.com>
2025-04-17 00:22:22 +0200

    quito el parse

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -6,7 +6,7 @@ import { de } from "vuetify/locale";
 export const useAuthStore = defineStore("auth", () => {
     const token = ref(null);
     const user = ref(null);
-    token.value = JSON.parse(localStorage.getItem("authToken")) || null;
+    token.value = localStorage.getItem("authToken") || null;
     user.value = JSON.parse(localStorage.getItem("user")) || null;
 
     const userService = useUserService();
