46e03ff8a2b830041f40b78cfc3a0ddd5d8a2e14
overdonebirch <alendez19@gmai.com>
2025-04-22 17:54:36 +0200

    añado el nombre y correo

--- src/views/auth/PerfilView.vue
+++ src/views/auth/PerfilView.vue
@@ -4,8 +4,12 @@ import { ref, reactive, computed, onMounted, watch } from 'vue'
 import Layout from '../../components/Layout.vue';
 import GlobalAlerts from '../../components/GlobalAlerts.vue';
 import { RouterView } from 'vue-router';
+import { useAuthStore } from '../../stores/authStore';
 
+const authStore = useAuthStore();
+const tab = ref(null);
 
+const user = ref(authStore.user);
 
 
 
@@ -26,12 +30,39 @@ import { RouterView } from 'vue-router';
                 </v-card-text>
             </v-card>
 
-            <!-- <div class="text-center pt-16" >
-                <v-progress-circular color="primary" indeterminate></v-progress-circular>
-            </div> -->
+
         </template>
         <template v-slot:body>
-           
+
+            <v-card class="w-50 roundedd-lg">
+
+                <v-tabs v-model="tab" bg-color="indigo-accent-1">
+                    <v-tab value="datos">Datos de usuario</v-tab>
+                    <v-tab value="seguridad">Seguridad </v-tab>
+                </v-tabs>
+                <v-card-text>
+                    <v-tabs-window v-model="tab">
+                        <v-tabs-window-item value="datos">
+                            <div class="ancho d-flex align-center justify-space-evenly centrar">
+                                <v-label class="ancho-label">Nombre : </v-label>
+                                <v-text-field class="ancho-input" bg-color="indigo-accent-2" disabled v-model="user.name"></v-text-field>
+                            </div> 
+                            <div class="ancho d-flex align-center justify-space-evenly centrar">
+                                <v-label class="ancho-label">Correo : </v-label>
+                                <v-text-field class="ancho-input " bg-color="indigo-accent-2" disabled v-model="user.email"></v-text-field>
+                            </div> 
+
+                        </v-tabs-window-item>
+                        <v-tabs-window-item value="seguridad">
+                            Seguridad
+                        </v-tabs-window-item>
+                    </v-tabs-window>
+                </v-card-text>
+
+
+            </v-card>
+
+
         </template>
     </Layout>
 
@@ -40,4 +71,17 @@ import { RouterView } from 'vue-router';
 
 
 
-<style lang="scss" scoped></style>
+<style  scoped>
+.ancho{
+    width: 400px;
+}
+.centrar{
+    margin : 0 auto;
+}
+.ancho-input{
+    width: 200px;
+}
+.ancho-label{
+    width: 100px;
+}
+</style>
